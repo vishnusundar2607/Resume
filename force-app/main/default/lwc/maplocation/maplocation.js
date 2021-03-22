4fad3be4b30aeed6d6cfef40b7c93307ba3d88c1
vishnu sundar <vishnusundar2607@gmail.com>
2021-03-22 16:55:26 +0530

    main

--- force-app/main/default/lwc/maplocation/maplocation.js
+++ force-app/main/default/lwc/maplocation/maplocation.js
@@ -1,4 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import messageChannel from '@salesforce/messageChannel/MapMessageChannel__c';
+import { subscribe, MessageContext } from 'lightning/messageService';
 
 export default class Maplocation extends LightningElement {
     mapMarkers = [{
@@ -8,6 +10,36 @@ export default class Maplocation extends LightningElement {
         }
     }];
 
+    @wire(MessageContext)
+    messageContext;
 
+    connectedCallback() {
+        this.handleSubscribe();
+    }
+
+    handleSubscribe() {
+        if (this.subscription) {
+            return;
+        }
+        this.subscription = subscribe(this.messageContext, messageChannel, (message) => {
+            console.log(message.messageText);
+            if (message.messageText == 'Home Address') {
+                this.mapMarkers = [{
+                    location: {
+                        Latitude: '10.374725',
+                        Longitude: '77.985593'
+                    }
+                }];
+            } else if (message.messageText == 'Current Address') {
+                this.mapMarkers = [{
+                    location: {
+                        Latitude: '13.021534',
+                        Longitude: '80.158730'
+                    }
+                }];
+            }
+
+        });
+    }
 
 }
--- force-app/main/default/lwc/personalDetails/personalDetails.js
+++ force-app/main/default/lwc/personalDetails/personalDetails.js
@@ -1,4 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import messageChannel from '@salesforce/messageChannel/MapMessageChannel__c';
+import { publish, MessageContext } from 'lightning/messageService'
 import PROFILE_PIC from '@salesforce/resourceUrl/testsvg';
 import PhoneIcon from '@salesforce/resourceUrl/PhoneIcon';
 import RESUME_ICONS from '@salesforce/resourceUrl/ResumeIcons';
@@ -11,17 +13,30 @@ export default class PersonalDetails extends LightningElement {
     { name: 'Skype', IconName: RESUME_ICONS + '/Skype.png', value: 'live:vishnusundar2607' },
     { name: 'LinkedIn', IconName: RESUME_ICONS + '/Linkedin.png', value: 'linkedin.com/in/vishnusundar2607' },
     { name: 'Trailhead', IconName: RESUME_ICONS + '/Trailhead.png', value: 'trailblazer.me/id/vishnusundar2607' },
+    { name: 'GitHub', IconName: RESUME_ICONS + '/GitHub.png', value: 'github.com/vishnusundar2607/Resume' },
     { name: 'Date Of Birth', IconName: RESUME_ICONS + '/DOB.png', value: '26/07/1992' },
     { name: 'Home Address', IconName: RESUME_ICONS + '/HomeLocation.png', value: '18 A M.V.M Nagar, Karur Road, Dindigul-624001' },
-    { name: 'Current Address', IconName: RESUME_ICONS + '/Currenlocation2.png', value: 'Srikrishna’s “VRINDAVAN” Flat No: G1, Plot No. 30, Nethaji Street, Sri Raja Rajeswari Nagar, Madhanadhapuram, Porur, Chennai-125' },
+    { name: 'Current Address', IconName: RESUME_ICONS + '/Currenlocation2.png', value: 'Srikrishna’s “VRINDAVAN” Flat No: G1, Plot No. 30, Nethaji Street, Sri Raja Rajeswari Nagar, Madhanadhapuram, Porur, Chennai-125' }
     ]
 
-   
-    checkvalue() {
-        
-        console.log(this.details);
+    @wire(MessageContext)
+    messageContext;
+
+    checkvalue(event) {
+
+        /* console.log(this.details);
         console.log(this.ResumeIcons);
-        console.log(details);
+        console.log(details); */
+        console.log(event.target.dataset.name);
+
+        let message = { messageText: event.target.dataset.name };
+
+        if (event.target.dataset.name == 'Home Address') {
+            publish(this.messageContext, messageChannel, message);
+        } else if (event.target.dataset.name == 'Current Address') {
+            publish(this.messageContext, messageChannel, message);
+        }
+
     }
 
 
