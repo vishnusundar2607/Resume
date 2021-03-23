ef0a9c07affd1d95670761d0bcee7ec4050dd727
vishnu sundar <vishnusundar2607@gmail.com>
2021-03-23 21:52:51 +0530

    main

--- force-app/main/default/lwc/skills/skills.js
+++ force-app/main/default/lwc/skills/skills.js
@@ -1,25 +1,25 @@
-import { LightningElement ,api,wire} from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import SKILLS from '@salesforce/apex/resumeController.getSkillList'
 
 export default class Skills extends LightningElement {
 
-    relative = false ;
-    skillheader ='';
-    skillDescription='';
+    relative = false;
+    skillheader = '';
+    skillDescription = '';
     @wire(SKILLS) skilllist;
-    skillSelected(event){
-        let selectedskill
-        if(this.skilllist!=null){
-            selectedskill = this.skilllist.data.find(rFields => rFields.Id === event.target.dataset.key);
-            this.skillheader =selectedskill.Name;
-            this.skillDescription =selectedskill.Description__c;
+
+    skillSelected(event) {
+        if (this.skilllist != null) {
+            let selectedskill = this.skilllist.data.find(rFields => rFields.Id === event.target.dataset.key);
+            this.skillheader = selectedskill.Name;
+            this.skillDescription = selectedskill.Description__c;
+            this.relative = true;
         }
-        this.relative = true;
+
     }
-    closeModal(){
+
+    closeModal() {
         this.relative = false;
     }
-    checkWire(){
-        console.log(this.skilllist);
-    }
+
 }
