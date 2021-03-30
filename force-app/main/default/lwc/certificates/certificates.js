9008a6114779c676e2924874d988230c2c4ad30f
vishnu sundar <vishnusundar2607@gmail.com>
2021-03-30 13:42:09 +0530

    main

--- force-app/main/default/lwc/certificates/certificates.js
+++ force-app/main/default/lwc/certificates/certificates.js
@@ -4,9 +4,9 @@ import CERTFICATE_PNG from '@salesforce/resourceUrl/CertificatePNG';
 export default class Certificates extends LightningElement {
 
     certArray = [
-        { name: 'Administrator', IconName: CERTFICATE_PNG + '/Administrator.png', credentialId: 21720821 },
+        { name: 'Platform Developer I', IconName: CERTFICATE_PNG + '/PlatformDeveloper1.png', credentialId: 21994594 },
         { name: 'Community Cloud Consultant', IconName: CERTFICATE_PNG + '/CommunityCloud.png', credentialId: 21836666 },
-        { name: 'Platform Developer I', IconName: CERTFICATE_PNG + '/CommunityCloud.png', credentialId: 21836666 },
+        { name: 'Administrator', IconName: CERTFICATE_PNG + '/Administrator.png', credentialId: 21720821 },
         { name: 'Platform Developer II', IconName: CERTFICATE_PNG + '/CommunityCloud.png', credentialId: 21836666 },
         { name: 'Javascript Developer', IconName: CERTFICATE_PNG + '/CommunityCloud.png', credentialId: 21836666 },
         { name: 'Platform App Builder', IconName: CERTFICATE_PNG + '/CommunityCloud.png', credentialId: 21836666 }];
--- force-app/main/default/lwc/recuriterMessage/recuriterMessage.js
+++ force-app/main/default/lwc/recuriterMessage/recuriterMessage.js
@@ -4,11 +4,13 @@ export default class RecuriterMessage extends LightningElement {
 
     ShowMessgae = false;
     handleSuccess() {
+        this.ToggleShowMessgae();
         const event = new ShowToastEvent({
             title: 'Your message saved succesfully',
             message: 'Email sent to your email address',
         });
         this.dispatchEvent(event);
+
     }
 
     ToggleShowMessgae() {
